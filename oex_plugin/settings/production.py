28ac150d8ab28294f37e5217e1c3d1a859aeac4b
Eugene Dyudyunov <evgen.dyudyunov@raccoongang.com>
2023-07-19 17:31:54 +0300

    fix: refactor after testing

--- oex_plugin/settings/production.py
+++ oex_plugin/settings/production.py
@@ -1,33 +1,28 @@
 """
 Production settings variables required by the RG OeX Plugin.
 """
-# The following settings are required in the context of the edx-platform
-# and should not be initialized when using the standalone plugin.
-from django.conf import settings
+from subprocess import CalledProcessError, check_output
+
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.django import DjangoIntegration
 
 
-AUTH_TOKENS = getattr(settings, 'AUTH_TOKENS', {})
+def plugin_settings(settings):
 
-# This is a safety measure that prevents errors from occurring when the plugin
-# is used outside of the platform environment without the necessary settings
-# that are typically obtained from within the platform.
-if AUTH_TOKENS.get('RG_SENTRY_DSN', None):
-    import sentry_sdk
-    import subprocess
-    from sentry_sdk.integrations.django import DjangoIntegration
-    from sentry_sdk.integrations.celery import CeleryIntegration
+    if RG_SENTRY_DSN := settings.AUTH_TOKENS.get('RG_SENTRY_DSN', None):
+        try:
+            platform_git_commit = check_output(['git', 'describe', '--always']).strip()
+        except (CalledProcessError, OSError):
+            platform_git_commit = ''
 
-    try:
-        platform_git_commit = subprocess.check_output(['git', 'describe', '--always']).strip()
-    except (subprocess.CalledProcessError, OSError):
-        platform_git_commit = ''
-    sentry_sdk.init(
-            AUTH_TOKENS.get('RG_SENTRY_DSN'),
+        sentry_sdk.init(
+            RG_SENTRY_DSN,
             auto_enabling_integrations=False,
             integrations=[DjangoIntegration(), CeleryIntegration()],
             environment=settings.ENV_TOKENS.get('RG_SENTRY_ENVIRONMENT', ''),
             release=platform_git_commit,
             send_default_pii=True
-            )
+        )
 
-INACTIVE_USER_URL = f'http{"s" if settings.HTTPS == "on" else ""}://{settings.CMS_BASE}'
+    settings.INACTIVE_USER_URL = f'http{"s" if settings.HTTPS == "on" else ""}://{settings.CMS_BASE}'
