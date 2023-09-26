29d98247b146fd31af55d596dda57eef18348bb7
Eugene Dyudyunov <evgen.dyudyunov@raccoongang.com>
2023-09-26 12:10:35 +0000

    fix: add missing import

--- oex_plugin/cms/urls.py
+++ oex_plugin/cms/urls.py
@@ -1,5 +1,6 @@
 from cms.urls import urlpatterns as patterns
 from django.urls import re_path
+
 from oex_plugin.openedx.core.djangoapps.common_views.branding import FaviconRedirectView
 
 # Add our URL path at the beggining so we'll use our view instead of the original one
